bcd609197189cd2ce5879d56a9bbc93552c0e0f2
Birb <Administrator@DESKTOP-8U774P0>
2024-02-01 13:32:32 +0800

    update

---
+++ stockXproj/Global_var.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Data.SqlClient
+Module Global_Var
+    Public conUMTC As New MySqlConnection
+    Public command As New MySqlCommand
+    Public sqlUMTCAdapter As New MySqlDataAdapter
+    Public DataUMTC As DataTable
+    Public strconnection, servername, databasename, databaseuserid,
+            databasepassword, action, port As String
+End Module
---
+++ stockXproj/Login.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Email_Field = New System.Windows.Forms.TextBox()
+        Me.Pass_Field = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Btn_Login = New System.Windows.Forms.Button()
+        Me.Btn_Cancel = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'Email_Field
+        '
+        Me.Email_Field.Location = New System.Drawing.Point(76, 65)
+        Me.Email_Field.Name = "Email_Field"
+        Me.Email_Field.Size = New System.Drawing.Size(175, 20)
+        Me.Email_Field.TabIndex = 0
+        '
+        'Pass_Field
+        '
+        Me.Pass_Field.Location = New System.Drawing.Point(76, 122)
+        Me.Pass_Field.Name = "Pass_Field"
+        Me.Pass_Field.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.Pass_Field.Size = New System.Drawing.Size(175, 20)
+        Me.Pass_Field.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(59, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Email"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(59, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password"
+        '
+        'Btn_Login
+        '
+        Me.Btn_Login.Location = New System.Drawing.Point(161, 164)
+        Me.Btn_Login.Name = "Btn_Login"
+        Me.Btn_Login.Size = New System.Drawing.Size(90, 33)
+        Me.Btn_Login.TabIndex = 4
+        Me.Btn_Login.Text = "Log-in"
+        Me.Btn_Login.UseVisualStyleBackColor = True
+        '
+        'Btn_Cancel
+        '
+        Me.Btn_Cancel.Location = New System.Drawing.Point(65, 164)
+        Me.Btn_Cancel.Name = "Btn_Cancel"
+        Me.Btn_Cancel.Size = New System.Drawing.Size(90, 33)
+        Me.Btn_Cancel.TabIndex = 5
+        Me.Btn_Cancel.Text = "Cancel"
+        Me.Btn_Cancel.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(111, 200)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(89, 13)
+        Me.LinkLabel1.TabIndex = 6
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Forgot Password."
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Btn_Cancel)
+        Me.Controls.Add(Me.Btn_Login)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Pass_Field)
+        Me.Controls.Add(Me.Email_Field)
+        Me.Name = "Login"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Email_Field As TextBox
+    Friend WithEvents Pass_Field As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Btn_Login As Button
+    Friend WithEvents Btn_Cancel As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
