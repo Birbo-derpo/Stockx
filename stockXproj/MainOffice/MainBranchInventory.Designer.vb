da54b2347b1468a8d7d8b17d1add593e58549e93
richard <My Pc@LAPTOP-P7IGKBSO>
2024-02-23 19:07:22 +0800

    latest update

--- stockXproj/MainOffice/MainBranchInventory.Designer.vb
+++ stockXproj/MainOffice/MainBranchInventory.Designer.vb
@@ -334,7 +334,7 @@ Partial Class MainBranchInventory
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(148, 0)
+        Me.ClientSize = New System.Drawing.Size(1912, 987)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
