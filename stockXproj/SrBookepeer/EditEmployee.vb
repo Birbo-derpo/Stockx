0af38c464a9ef92931c3ecdb214bdc87aa753a7d
richard <My Pc@LAPTOP-P7IGKBSO>
2024-03-22 02:37:36 +0800

    inventory manager can changepassword and other fix

--- stockXproj/SrBookepeer/EditEmployee.vb
+++ stockXproj/SrBookepeer/EditEmployee.vb
@@ -2,13 +2,27 @@
 
 Public Class EditEmployee
     Private datUMTC As DataTable
+
+
+    Private Sub editEmployee()
+
+
+
+    End Sub
     Private Sub Create_Click(sender As Object, e As EventArgs) Handles Create.Click
         Try
 
             If Not txtGmail.Text.Contains("@") Then
                 MessageBox.Show("Invalid email address format. Please include '@' sign.", "Invalid Email", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
                 Return
             End If
+
+            If String.IsNullOrWhiteSpace(txtGmail.Text) OrElse String.IsNullOrWhiteSpace(txtFirst.Text) OrElse String.IsNullOrWhiteSpace(txtLast.Text) Then
+                MessageBox.Show("Please input all the credentials.", "Missing Information", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
 
             sqlUMTCAdapter = New MySqlDataAdapter
             datUMTC = New DataTable
@@ -23,30 +37,30 @@ Public Class EditEmployee
                 .Parameters.AddWithValue("@p_Last", txtLast.Text)
                 .Parameters.AddWithValue("@p_Position", cmbPosition.Text)
                 .Parameters.AddWithValue("@p_Gender", cmbGender.Text)
-
                 .ExecuteNonQuery()
+
+
+
+
+                MessageBox.Show("Change successful.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
             End With
 
 
-            MessageBox.Show("Record successfully saved", "Saving Record", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+
 
 
             txtGmail.Clear()
-            txtFirst.Clear()
-            txtLast.Clear()
-            cmbPosition.SelectedIndex = -1
-            cmbGender.SelectedIndex = -1
+                txtFirst.Clear()
+                txtLast.Clear()
+                cmbPosition.SelectedIndex = -1
+                cmbGender.SelectedIndex = -1
 
-            Me.Dispose()
-            Display_Employee.Show()
+                Me.Dispose()
+                Display_Employee.Show()
 
-        Catch ex As MySqlException
 
-            If ex.Number = 1644 Then
-                MessageBox.Show("Email address already exists . Please use a different email address.", "Duplicate Email", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            Else
-                MessageBox.Show("An error occurred while saving the record: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            End If
+
 
         Catch ex As Exception
 
