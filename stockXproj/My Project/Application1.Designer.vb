dfdb8ac9fc341bf6e5376f33d9e629b63991158a
richard <My Pc@LAPTOP-P7IGKBSO>
2024-04-03 15:03:19 +0800

    Addnewunit done

--- stockXproj/My Project/Application1.Designer.vb
+++ stockXproj/My Project/Application1.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.stockXproj.addnewunits
+            Me.MainForm = Global.stockXproj.addMotorcycle
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
